e0f393a89dce4cb9be9aab22735dd3d436dcf5bf
cheshire <my@email.com>
2019-04-21 23:56:45 +0300

    project start

---
+++ forms.py
@@ -0,0 +1,63 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField,\
+    BooleanField, SubmitField, IntegerField, SelectField, FileField, TextAreaField
+from wtforms.validators import DataRequired, Email
+import os
+
+
+class LoginForm(FlaskForm):
+    """Форма авторизации"""
+    username = StringField('Логин', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegisterForm(FlaskForm):
+    """Форма регистрации"""
+    user_name = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Email адрес', validators=[DataRequired(), Email()])
+    password_hash = PasswordField('Пароль', validators=[DataRequired()])
+    confirm = PasswordField('Повторите пароль', validators=[DataRequired()])
+    accept_tos = BooleanField('Я принимаю лицензионное соглашение', validators=[DataRequired()])
+    submit = SubmitField('Создать учетную запись')
+
+
+class AddCarForm(FlaskForm):
+    name = StringField('Название')#, validators=[DataRequired()])
+    price = IntegerField('Цена')#, validators=[DataRequired()])
+    adress = IntegerField('Адрес')#, validators=[DataRequired()])
+    country = SelectField('Страна', coerce=int)  # , validators=[DataRequired()])
+    description = TextAreaField('Описание')#, validators=[DataRequired()])
+    image = FileField('Фото отеля')
+
+    submit = SubmitField('Создать')
+
+    def __init__(self):
+        super(AddCarForm, self).__init__()
+
+    def save_im(self):
+        im = self.image.data
+        print(im)
+        im_name = os.path.join('static', 'img', im.filename)
+        im.save(im_name)
+
+
+class AddDealerForm(FlaskForm):
+    """Добавление дилерского центра"""
+    name = StringField('Название', validators=[DataRequired()])
+    address = StringField('Адрес', validators=[DataRequired()])
+    submit = SubmitField('Добавить дилерский центр')
+
+
+class SearchPriceForm(FlaskForm):
+    """Форма поиска по цене"""
+    start_price = IntegerField('Минимальная цена', validators=[DataRequired()], default=500000)
+    end_price = IntegerField('Максимальная цена', validators=[DataRequired()], default=1000000)
+    submit = SubmitField('Поиск')
+
+
+class SearchDealerForm(FlaskForm):
+    """Форма поиска по дилерскому центру"""
+    dealer_id = SelectField('Номер дилерского центра', coerce=int, validators=[DataRequired()])
+    submit = SubmitField('Поиск')
---
+++ models.py
@@ -0,0 +1,178 @@
+class UsersModel:
+    """Сущность пользователей"""
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        """Инициализация таблицы"""
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS users 
+                            (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             user_name VARCHAR(20) UNIQUE,
+                             password_hash VARCHAR(128),
+                             email VARCHAR(20),
+                             is_admin INTEGER
+                             )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, user_name, password_hash, email, is_admin=False):
+        """Вставка новой записи"""
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO users 
+                          (user_name, password_hash, email, is_admin) 
+                          VALUES (?,?,?,?)''',
+                       (user_name, password_hash, email, int(is_admin)))
+        cursor.close()
+        self.connection.commit()
+
+    def exists(self, user_name):
+        """Проверка, есть ли пользователь в системе"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE user_name = ?", [user_name])
+        row = cursor.fetchone()
+        return (True, row[2], row[0]) if row else (False,)
+
+    def get(self, user_id):
+        """Возврат пользователя по id"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE id = ?", (str(user_id)))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        """Запрос всех пользователей"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
+        return rows
+
+
+class DealersModel:
+    """Сущность дилерских центров"""
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        """Инициализация таблицы"""
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS dealers 
+                            (dealer_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             name VARCHAR(20) UNIQUE,
+                             address VARCHAR(128)
+                        )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, name, address):
+        """Добавление дилерского центра"""
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO dealers 
+                          (name, address) 
+                          VALUES (?,?)''',
+                       (name, address))
+        cursor.close()
+        self.connection.commit()
+
+    def exists(self, name):
+        """Поиск дилерского центра по названию"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM dealers WHERE name = ?",
+                       name)
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+
+    def get(self, dealer_id):
+        """Запрос дилерского центра по id"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM dealers WHERE dealer_id = ?", (str(dealer_id)))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        """Запрос всех дилерских центров"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM dealers")
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, dealer_id):
+        """Удаление дилерского центра"""
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM dealers WHERE dealer_id = ?''', (str(dealer_id)))
+        cursor.close()
+        self.connection.commit()
+
+
+class CarsModel:
+    """Сущность автомобилей"""
+    def __init__(self, connection):
+        self.connection = connection
+
+    def init_table(self):
+        """Инициализация таблицы"""
+        cursor = self.connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS cars 
+                            (car_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                             model VARCHAR(20),
+                             price INTEGER,
+                             power VARCHAR(100),
+                             color VARCHAR(2000),
+                             dealer INTEGER,
+                             image VARCHAR(1024)
+                        )''')
+        cursor.close()
+        self.connection.commit()
+
+    def insert(self, model, price, power, color, dealer, image):
+        """Добавление автомобиля"""
+        cursor = self.connection.cursor()
+        cursor.execute('''INSERT INTO cars 
+                          (model, price, power, color, dealer, image) 
+                          VALUES (?,?,?,?,?,?)''',
+                       (model, str(price), str(power), color, str(dealer), image))
+        cursor.close()
+        self.connection.commit()
+
+    def exists(self, model):
+        """Поиск автомобиля по модели"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM cars WHERE model = ?",
+                       model)
+        row = cursor.fetchone()
+        return (True, row[0]) if row else (False,)
+
+    def get(self, car_id):
+        """Поиск автомобиля по id"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT * FROM cars WHERE car_id = ?", (str(car_id),))
+        row = cursor.fetchone()
+        return row
+
+    def get_all(self):
+        """Запрос всех автомобилей"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT model, price, car_id, image FROM cars")
+        rows = cursor.fetchall()
+        return rows
+
+    def delete(self, car_id):
+        """Удаление автомобиля"""
+        cursor = self.connection.cursor()
+        cursor.execute('''DELETE FROM cars WHERE car_id = ?''', (str(car_id),))
+        cursor.close()
+        self.connection.commit()
+
+    def get_by_price(self, start_price, end_price):
+        """Запрос автомобилей по цене"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT model, price, car_id, image FROM cars WHERE price >= ? AND price <= ?", (str(start_price), str(end_price)))
+        row = cursor.fetchall()
+        return row
+
+    def get_by_dealer(self, dealer_id):
+        """Запрос автомобилей по дилерскому центру"""
+        cursor = self.connection.cursor()
+        cursor.execute("SELECT model, price, car_id, image FROM cars WHERE dealer = ?", (str(dealer_id)))
+        row = cursor.fetchall()
+        return row
